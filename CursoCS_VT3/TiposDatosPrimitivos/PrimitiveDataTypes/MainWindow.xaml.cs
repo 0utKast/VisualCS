31b0a33172e482d95b19b70c752ba5eb6e24c41d
0utKast <educaleft@gmail.com>
2013-04-11 16:00:40 +0100

    VT3_VISUALCS2012
    Archivos de prácticas del Videotutorial nº 3 del Curso de Visual C SHARP
    2012, dedicado a las variables.

---
+++ CursoCS_VT3/TiposDatosPrimitivos/PrimitiveDataTypes/MainWindow.xaml.cs
@@ -0,0 +1,123 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace PrimitiveDataTypes
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void typeSelectionChanged(object sender, SelectionChangedEventArgs e)
+        {
+            ListBoxItem selectedType = (type.SelectedItem as ListBoxItem);
+            switch (selectedType.Content.ToString())
+            {
+                case "int":
+                    showIntValue();
+                    break;
+                case "long":
+                    showLongValue();
+                    break;
+                case "float":
+                    showFloatValue();
+                    break;
+                case "double":
+                    showDoubleValue();
+                    break;
+                case "decimal":
+                    showDecimalValue();
+                    break;
+                case "string":
+                    showStringValue();
+                    break;
+                case "char":
+                    showCharValue();
+                    break;
+                case "bool":
+                    showBoolValue();
+                    break;
+            }
+        }
+
+        private void showIntValue()
+        {
+            int intVar;
+            intVar = 42;
+            value.Text = intVar.ToString();
+        }
+
+        private void showLongValue()
+        {
+            long longVar;
+            longVar = 42L;
+            value.Text = longVar.ToString();
+        }
+
+        private void showFloatValue()
+        {
+            float floatVar;
+            floatVar = 0.42F;
+            value.Text = floatVar.ToString();
+        }
+
+        private void showDoubleValue()
+        {
+            double doubleVar;
+            doubleVar = 0.42;
+            value.Text = doubleVar.ToString();
+
+        }
+
+        private void showDecimalValue()
+        {
+            decimal decimalVar;
+            decimalVar = 0.42M;
+            value.Text = decimalVar.ToString();
+        }
+
+        private void showStringValue()
+        {
+            string stringVar;
+            stringVar = "Cuarenta y dos";
+            value.Text = stringVar; // ToString not needed
+        }
+
+        private void showCharValue()
+        {
+            char charVar;
+            charVar = 'x';
+            value.Text = charVar.ToString();
+        }
+
+        private void showBoolValue()
+        {
+            bool boolVar;
+            boolVar = false;
+            value.Text = boolVar.ToString();
+
+        }
+
+        private void quitClick(object sender, RoutedEventArgs e)
+        {
+            this.Close();
+        }
+    }
+}
