e8612320ae6099a0e81935d14c8573cae0e50293
0utKast <educaleft@gmail.com>
2013-04-14 15:52:02 +0100

    VT4_CursoVisualCS
    Archivos de prácticas Videotutorial Nº 4 del Curso de Visual C# 2012

---
+++ CursoCS_VT4/OperadoresMatematicos/MathsOperators/MainWindow.xaml.cs
@@ -0,0 +1,120 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace MathsOperators
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void calculateClick(object sender, RoutedEventArgs e)
+        {
+            try
+            {
+                if ((bool)addition.IsChecked)
+                {
+                    addValues();
+                }
+                else if ((bool)subtraction.IsChecked)
+                {
+                    subtractValues();
+                }
+                else if ((bool)multiplication.IsChecked)
+                {
+                    multiplyValues();
+                }
+                else if ((bool)division.IsChecked)
+                {
+                    divideValues();
+                }
+                else if ((bool)remainder.IsChecked)
+                {
+                    remainderValues();
+                }
+            }
+            catch (Exception caught)
+            {
+                expression.Text = "";
+                result.Text = caught.Message;
+            }
+        }
+
+        private void addValues()
+        {
+            int lhs = int.Parse(lhsOperand.Text);
+            int rhs = int.Parse(rhsOperand.Text);
+            int outcome = 0;
+            // Hacer: Suma rhs a lhs y almacena el resultado in la variable outcome
+            outcome = lhs + rhs;
+            expression.Text = lhsOperand.Text + " + " + rhsOperand.Text;
+            result.Text = outcome.ToString();
+        }
+
+        private void subtractValues()
+        {
+            int lhs = int.Parse(lhsOperand.Text);
+            int rhs = int.Parse(rhsOperand.Text);
+            int outcome = 0;
+            // Hacer: Resta rhs de lhs and almacena el resultado en outcome
+            outcome = lhs - rhs;
+            expression.Text = lhsOperand.Text + " - " + rhsOperand.Text;
+            result.Text = outcome.ToString();
+        }
+
+        private void multiplyValues()
+        {
+            int lhs = int.Parse(lhsOperand.Text);
+            int rhs = int.Parse(rhsOperand.Text);
+            int outcome = 0;
+            // Hacer: Multiplica lhs por rhs y almacena el resultado en outcome
+            outcome = lhs * rhs;
+            expression.Text = lhsOperand.Text + " * " + rhsOperand.Text;
+            result.Text = outcome.ToString();
+        }
+
+        private void divideValues()
+        {
+            int lhs = int.Parse(lhsOperand.Text);
+            int rhs = int.Parse(rhsOperand.Text);
+            int outcome = 0;
+            // Hacer: Divide lhs por rhs y almacena el resultado en outcome
+            outcome = lhs / rhs;
+            expression.Text = lhsOperand.Text + " / " + rhsOperand.Text;
+            result.Text = outcome.ToString();
+        }
+
+        private void remainderValues()
+        {
+            int lhs = int.Parse(lhsOperand.Text);
+            int rhs = int.Parse(rhsOperand.Text);
+            int outcome = 0;
+            // Hacer: Devuelve el resto después de dividir lhs por rhs y almacena el resultado en outcome
+            outcome = lhs % rhs;
+            expression.Text = lhsOperand.Text + " % " + rhsOperand.Text;
+            result.Text = outcome.ToString();
+        }
+
+        private void quitClick(object sender, RoutedEventArgs e)
+        {
+            this.Close();
+        }
+    }
+}
