f3f03bf15a3257ac1c9c7ccb9cdfb27d042b0f41
0utKast <educaleft@gmail.com>
2013-04-25 22:47:43 +0100

    VT7_CursoCSharp
    VideoTutorial número 7 del Curso de Visual CSharp 2012. Parámetros
    opcionales y argumentos con nombre.

---
+++ CursoCS_VT7/TarifaDiaria/TarifaDiaria/Program.cs
@@ -0,0 +1,54 @@
+﻿using System;
+
+namespace TarifaDiaria
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            (new Program()).run();
+        }
+
+        void run()
+        {
+            //double tarifa = calcularTarifa();
+            //double tarifa = calcularTarifa(435.23);
+            //double tarifa = calcularTarifa(435.23, 5);
+            //double tarifa = calcularTarifa(laTarifaDiaria: 312.2);
+            //double tarifa = calcularTarifa(numDeDias: 5);
+            double tarifa = calcularTarifa(tarifaDiaria: 312.2);
+                      
+            Console.WriteLine("Tarifa es {0}", tarifa);
+        }
+
+        /*private double calcularTarifa(double laTarifaDiaria = 500.0, int numDeDias = 1)
+        {
+            Console.WriteLine("calcular tarifa usando dos parámetros opcionales");
+            return laTarifaDiaria * numDeDias;
+        }*/
+
+        private double calcularTarifa(double tarifaDiaria = 500.0, int numDeDias = 1)
+        {
+            Console.WriteLine("calcular tarifa usando dos parámetros opcionales");
+            return tarifaDiaria * numDeDias;
+        }
+
+        private double calcularTarifa(double laTarifaDiaria = 500.0)
+        {
+            Console.WriteLine("calcular tarifa usando un parámetro opcional");
+            int numDiasPorDefecto = 1;
+            return laTarifaDiaria * numDiasPorDefecto;
+        }
+
+        private double calcularTarifa()
+        {
+            Console.WriteLine("calcular Tarifa usando valores de código duro");
+            double tarifaDiariaPorDefecto = 400.0;
+            int numDiasPorDefecto = 1;
+            return tarifaDiariaPorDefecto * numDiasPorDefecto;
+        }
+
+
+
+    }
+}
