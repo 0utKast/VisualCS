4fed674d82b99efba20d963faa1facfa40c5b4bb
0utKast <educaleft@gmail.com>
2013-05-14 20:26:48 +0100

    VT10_VSC
    Archivos de prácticas Videotutorial nº 10 Visual CSharp 2012

---
+++ CursoCS_VT10/DeclaracionWhile/DeclaracionWhile/MainWindow.xaml.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using Microsoft.Win32;
+using System.IO;
+
+namespace DeclaracionWhile
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+
+    public partial class MainWindow : Window
+    {
+        private OpenFileDialog openFileDialog = null;
+
+        public MainWindow()
+        {
+            InitializeComponent();
+            openFileDialog = new OpenFileDialog();
+            openFileDialog.FileOk += openFileDialogFileOk;
+        }
+
+        private void openFileClick(object sender, RoutedEventArgs e)
+        {
+            openFileDialog.ShowDialog();
+        }
+
+        private void openFileDialogFileOk(object sender, System.ComponentModel.CancelEventArgs e)
+        {
+            string fullPathname = openFileDialog.FileName;
+            FileInfo src = new FileInfo(fullPathname);
+            fileName.Text = src.FullName;
+            TextReader reader = src.OpenText();
+            displayData(reader);
+        }
+
+        private void displayData(TextReader reader)
+        {
+            source.Text = "";
+            string line = reader.ReadLine();
+            while (line != null)
+            {
+                source.Text += line + '\n';
+                line = reader.ReadLine();
+            }
+            reader.Close();
+
+        }
+    }
+}
